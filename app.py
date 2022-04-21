759d1ac5a5b7e4c3cd6e66ec270ff7c26899d476
michael <michael.mechenko@gmail.com>
2022-04-21 19:28:38 -0400

    Create frames for windows

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # Michael Mechenko EECE2140
 
+from ctypes import resize
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo
-
+import random
+import time
 
 class TileMatching(tk.Tk):
     def __init__(self):
@@ -11,9 +13,14 @@ class TileMatching(tk.Tk):
 
         # configure the root window
         self.title('Tile Matching Memory Game')
-        self.geometry('800x600')
+        self.geometry('800x600', resize=False)
 
-        # label
+    def button_clicked(self):
+        showinfo(title='Information',
+                 message='Hello, Tkinter!')
+    
+    def create_window(self):
+         # label
         self.label = ttk.Label(self, text='Hello, Tkinter!')
         self.label.pack()
 
@@ -21,19 +28,20 @@ class TileMatching(tk.Tk):
         self.button = ttk.Button(self, text='Click Me')
         self.button['command'] = self.button_clicked
         self.button.pack()
-        
 
-    def button_clicked(self):
-        showinfo(title='Information',
-                 message='Hello, Tkinter!')
-    
-    def create_grid(self, size):
-         for i in range(size):
-             for j in range(size):
-                 self.Button(self, text='{}x{}'.format(i, j)).grid(row=i, column=j).pack()
+def main():
+    root = TileMatching()
+    root.create_window()
+    tab_control = ttk.Notebook(root)
+    easy = ttk.Frame(tab_control)
+    medium = ttk.Frame(tab_control)
+    hard = ttk.Frame(tab_control)
+    tab_control.add(easy, text='Easy')
+    tab_control.add(medium, text='Medium')
+    tab_control.add(hard, text='Hard')
+    tab_control.pack(expand=1, fill='both')
+    root.mainloop()
 
 
 if __name__ == "__main__":
-    TileMatching = TileMatching()
-    TileMatching.mainloop()
-    TileMatching.create_grid(10)
+    main()
